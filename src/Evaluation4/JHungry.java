af98f74b50481ae347c54aac269692df82465cec
KratiTare-coditas <124657680+KratiTare-coditas@users.noreply.github.com>
2023-02-24 16:59:04 +0530

    Add files via upload

---
+++ src/Evaluation4/JHungry.java
@@ -0,0 +1,14 @@
+package Evaluation4;
+
+public class JHungry {
+    public static void main(String[] args) {
+        try{
+            System.out.println("A");
+            int num=99/0;
+            System.out.println("B");
+        }
+        catch(ArithmeticException e){
+            
+        }
+    }
+}
